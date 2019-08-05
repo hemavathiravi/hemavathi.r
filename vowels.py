5a173cc49484bd09f0ca9f1325888ebb24c95ed2
hemavathiravi <52271619+hemavathiravi@users.noreply.github.com>
2019-08-05 15:09:38 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,5 @@
+ch = input("please enter your own character :")
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' ):
+      printf(" character ", ch, "is a vowel")
+else:
+	printf(" character ", ch,"is a constant")
