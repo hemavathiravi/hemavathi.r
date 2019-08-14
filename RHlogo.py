c81d32463057aa0210437b30346c8f739f724e0f
hemavathiravi <52271619+hemavathiravi@users.noreply.github.com>
2019-08-14 15:56:49 +0530

    Create RHlogo.py

---
+++ RHlogo.py
@@ -0,0 +1,13 @@
+#hema
+r,h,v=map(str,input().split())
+r=list(r)
+h=list(h)
+v=int(v)
+count=0
+for i in range(0,len(r)):
+        if(n[i]!=k[i]):
+            count=count+1
+if(count==v):
+    print("yes")
+else:
+    print("no")
